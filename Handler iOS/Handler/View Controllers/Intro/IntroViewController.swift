848e73b123fa61654393106313bcfeb90e1a220c
Christian Praiß <christian_praiss@icloud.com>
2015-11-23 14:05:01 +0100

    new onboarding screens

--- Handler iOS/Handler/View Controllers/Intro/IntroViewController.swift
+++ Handler iOS/Handler/View Controllers/Intro/IntroViewController.swift
@@ -13,7 +13,6 @@ class IntroViewController: UIViewController, UIScrollViewDelegate {
 	@IBOutlet var firstView: UIView!
 	@IBOutlet var secondView: UIView!
 	@IBOutlet var thirdView: UIView!
-	@IBOutlet var fourthView: UIView!
 	var contentView: UIView = UIView()
 	@IBOutlet weak var pageControl: UIPageControl!
 	@IBOutlet var scrollView: UIScrollView!
@@ -21,31 +20,27 @@ class IntroViewController: UIViewController, UIScrollViewDelegate {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-        let layer = CAGradientLayer.gradientLayerForBounds(UIScreen.mainScreen().bounds)
-        self.view.layer.insertSublayer(layer, atIndex: 0)
         
-		totalWidth = Double(UIScreen.mainScreen().bounds.width * 4)
-		contentView = UIView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width * 4, UIScreen.mainScreen().bounds.height))
+		totalWidth = Double(UIScreen.mainScreen().bounds.width * 3)
+		contentView = UIView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width * 3, UIScreen.mainScreen().bounds.height))
 		firstView.frame = CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
 		secondView.frame = CGRectMake(firstView.bounds.width, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
 		thirdView.frame = CGRectMake(firstView.bounds.width * 2, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
-		fourthView.frame = CGRectMake(firstView.bounds.width * 3, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height)
 
 		contentView.addSubview(thirdView)
-		contentView.addSubview(fourthView)
 		contentView.addSubview(secondView)
 		contentView.addSubview(firstView)
 		
 		scrollView.addSubview(contentView)
 
-		scrollView.contentSize = CGSizeMake(UIScreen.mainScreen().bounds.width * 4, UIScreen.mainScreen().bounds.height)
+		scrollView.contentSize = CGSizeMake(UIScreen.mainScreen().bounds.width * 3, UIScreen.mainScreen().bounds.height)
 	}
 	
 	@IBAction func beginButtonPressed(sender: RoundedBorderButton) {
 		scrollView.setContentOffset(CGPointMake(firstView.bounds.width, 0), animated: true)
 	}
 	
-	@IBAction func finishWalkthrough(sender: UIButton) {
+	@IBAction func finishWalkthrough(sender: AnyObject) {
 		NSUserDefaults.standardUserDefaults().setBool(true, forKey: "didFinishWalkthrough")
 		NSUserDefaults.standardUserDefaults().synchronize()
 		UIView.transitionWithView(AppDelegate.sharedInstance().window!, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
@@ -63,7 +58,14 @@ class IntroViewController: UIViewController, UIScrollViewDelegate {
 	
 	func scrollViewDidScroll(scrollView: UIScrollView) {
 		if scrollView.contentOffset.x > 0 {
-			pageControl.currentPage = Int((Double(scrollView.contentOffset.x) / totalWidth )*4)
+			pageControl.currentPage = Int((Double(scrollView.contentOffset.x) / totalWidth )*3)
+            
+            if pageControl.currentPage == 2 {
+                var token: dispatch_once_t = 08
+                dispatch_once(&token) { () -> Void in
+                    NSRunLoop.mainRunLoop().addTimer(NSTimer(timeInterval: 3, target: self, selector: "finishWalkthrough:", userInfo: nil, repeats: false), forMode: NSRunLoopCommonModes)
+                }
+            }
 		}else{
 			pageControl.currentPage = 0
 		}
