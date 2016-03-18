d3936c7406ba73d31b76280ee3fe79ac6468a1e5
Cagdas Altinkaya <cagdas@rgbm.co>
2016-03-18 19:28:08 +0200

    Formatting

--- Handler iOS/Handler/Database/Models/Thread.swift
+++ Handler iOS/Handler/Database/Models/Thread.swift
@@ -10,72 +10,72 @@ import Foundation
 import CoreData
 
 class Thread: NSManagedObject {
-    
-    class func fromID(id: String, inContext: NSManagedObjectContext?) -> Thread? {
-        var thread: Thread?
-        let context = inContext ?? MailDatabaseManager.sharedInstance.backgroundContext
-        if let request = self.fetchRequestForID(id){
-            do {
-                if let threads = try context.executeFetchRequest(request) as? [Thread], let foundthread = threads.first {
-                    thread = foundthread
-                }
-            } catch {
-                print(error)
-            }
-        }
-        
-        if let thread = thread {
-            return thread
-        }else {
-            let createdthread = Thread(managedObjectContext: context ?? MailDatabaseManager.sharedInstance.backgroundContext)
-            createdthread.id = id
-            return createdthread
-        }
-    }
-    
-    func updateInbox(){
-        var show = false
-        if let messages = self.messages {
-            for message in messages.allObjects as! [Message] {
-                if message.isInbox {
-                    show = true
-                }
-            }
-        }
-        self.showInInbox = NSNumber(bool: show)
-        
-        MailDatabaseManager.sharedInstance.saveBackgroundContext()
-    }
-    
-    var mostRecentMessage: Message? {
-        let msgSet = NSSet(set: messages!)
-        let messageList = msgSet.allObjects as? [Message]
-        let sorted =  messageList?.sort({
-            if let firstSent = $0.sent_at, let secondSent = $1.sent_at {
-                return firstSent.compare(secondSent) == NSComparisonResult.OrderedDescending
-            }
-            return true
-        })
-        return sorted?.first
-    }
-    
-    func archive() {
-        if let messages = self.messages {
-            for message in messages {
-                if let m = message as? Message {
-                    m.moveToArchive()
-                }
-            }
-        }
-    }
-    
-    func unarchive() {
-        if let messages = self.messages {
-            for message in messages {
-                if let m = message as? Message {
-                    m.moveToInbox()
-                }
-            }
-        }
-    }
+
+	class func fromID(id: String, inContext: NSManagedObjectContext?) -> Thread? {
+		var thread: Thread?
+		let context = inContext ?? MailDatabaseManager.sharedInstance.backgroundContext
+		if let request = self.fetchRequestForID(id){
+			do {
+				if let threads = try context.executeFetchRequest(request) as? [Thread], let foundthread = threads.first {
+					thread = foundthread
+				}
+			} catch {
+				print(error)
+			}
+		}
+
+		if let thread = thread {
+			return thread
+		}else {
+			let createdthread = Thread(managedObjectContext: context ?? MailDatabaseManager.sharedInstance.backgroundContext)
+			createdthread.id = id
+			return createdthread
+		}
+	}
+
+	func updateInbox(){
+		var show = false
+		if let messages = self.messages {
+			for message in messages.allObjects as! [Message] {
+				if message.isInbox {
+					show = true
+				}
+			}
+		}
+		self.showInInbox = NSNumber(bool: show)
+
+		MailDatabaseManager.sharedInstance.saveBackgroundContext()
+	}
+
+	var mostRecentMessage: Message? {
+		let msgSet = NSSet(set: messages!)
+		let messageList = msgSet.allObjects as? [Message]
+		let sorted =  messageList?.sort({
+			if let firstSent = $0.sent_at, let secondSent = $1.sent_at {
+				return firstSent.compare(secondSent) == NSComparisonResult.OrderedDescending
+			}
+			return true
+		})
+		return sorted?.first
+	}
+
+	func archive() {
+		if let messages = self.messages {
+			for message in messages {
+				if let m = message as? Message {
+					m.moveToArchive()
+				}
+			}
+		}
+	}
+
+	func unarchive() {
+		if let messages = self.messages {
+			for message in messages {
+				if let m = message as? Message {
+					m.moveToInbox()
+				}
+			}
+		}
+	}
 }
