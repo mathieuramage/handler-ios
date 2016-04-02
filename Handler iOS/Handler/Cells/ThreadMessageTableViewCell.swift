ff11737616e77d703763d0f591a2a676c174eeec
Ryniere <ryniere16@gmail.com>
2016-04-02 13:24:25 -0300

    IOS-218 separator lines

--- Handler iOS/Handler/Cells/ThreadMessageTableViewCell.swift
+++ Handler iOS/Handler/Cells/ThreadMessageTableViewCell.swift
@@ -9,20 +9,30 @@
 import UIKit
 
 class ThreadMessageTableViewCell: UITableViewCell {
-    
-    @IBOutlet var senderLabel: UILabel!
-    @IBOutlet var senderHandleButton: UIButton!
-    @IBOutlet var recipientLabel: UILabel!
-    @IBOutlet var timeStampeLabel: UILabel!
-    @IBOutlet var senderImageView: UIImageView!
-    @IBOutlet var contentTextView: UITextView!
-    @IBOutlet var separatorContainerHeightConstraint: NSLayoutConstraint!
+	
+	@IBOutlet var senderLabel: UILabel!
+	@IBOutlet var senderHandleButton: UIButton!
+	@IBOutlet var recipientLabel: UILabel!
+	@IBOutlet var timeStampeLabel: UILabel!
+	@IBOutlet var senderImageView: UIImageView!
+	@IBOutlet var contentTextView: UITextView!
+	@IBOutlet var separatorContainerHeightConstraint: NSLayoutConstraint!
+	@IBOutlet var recipientDividerView: UIView!
+	@IBOutlet var messageDividerView: UIView!
+	@IBOutlet var separatorLineView: UIView!
 
-    var sender: User?
+	var sender: User?
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		self.recipientDividerView.backgroundColor = UIColor(rgba: HexCodes.lightGray)
+		self.messageDividerView.backgroundColor = UIColor(rgba: HexCodes.lightGray)
+		self.separatorLineView.backgroundColor = UIColor(rgba: HexCodes.lighterGray)
+	}
 
-    @IBAction func didPressUsername(sender: UIButton) {
-        if let sender = self.sender {
-            ContactCardViewController.showWithUser(sender)
-        }
-    }
+	@IBAction func didPressUsername(sender: UIButton) {
+		if let sender = self.sender {
+			ContactCardViewController.showWithUser(sender)
+		}
+	}
 }
