d73af838148845429a931cceb374db76d1b30383
Otavio Lima <otaviosilvalima@gmail.com>
2016-04-09 23:40:44 +0300

    Fix tabs

--- Handler iOS/Handler/Plugins/Formatting Plugins/ConversationContentCellFormatter.swift
+++ Handler iOS/Handler/Plugins/Formatting Plugins/ConversationContentCellFormatter.swift
@@ -9,57 +9,57 @@
 import UIKit
 
 struct ConversationContentFormatter: MessageContentCellFormatter {
-    
-    var timeFormatter: NSDateFormatter = {
-        let formatter = NSDateFormatter()
-        formatter.dateStyle = NSDateFormatterStyle.LongStyle
-        formatter.timeStyle = NSDateFormatterStyle.ShortStyle
-        return formatter
-    }()
+	
+	var timeFormatter: NSDateFormatter = {
+		let formatter = NSDateFormatter()
+		formatter.dateStyle = NSDateFormatterStyle.LongStyle
+		formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+		return formatter
+	}()
 
-    func populateView(data message: Message?, view: ThreadMessageTableViewCell) {
+	func populateView(data message: Message?, view: ThreadMessageTableViewCell) {
 		view.richTextContent.editingEnabled = false
-        view.richTextContent.setHTML(message?.content ?? "No content")
+		view.richTextContent.setHTML(message?.content ?? "No content")
 		view.contentHeightConstraint.constant = CGFloat(view.richTextContent.editorHeight)
 		view.richTextContent.webView.dataDetectorTypes = [.All]
 		view.richTextContent.backgroundColor = UIColor.clearColor()
 		view.richTextContent.webView.backgroundColor = UIColor.clearColor()
 		view.richTextContent.webView.scrollView.backgroundColor = UIColor.clearColor()
 		view.richTextContent.webView.opaque = false
-        view.senderLabel.text = message?.sender?.name
-        view.senderHandleButton.setTitle("@" + (message?.sender?.handle ?? ""), forState: .Normal)
-        if let recipient = message?.recipients?.allObjects.first as? User, let displayName = recipient.name, let handle = recipient.handle {
-            view.recipientLabel.text = "To: " + displayName + " @" + handle
-        }
-        else {
-            view.recipientLabel.text = "To: -"
-        }
-        view.sender = message?.sender
-        view.timeStampeLabel.text = timeFormatter.stringFromDate(message?.sent_at ?? NSDate())
-        if let string = message?.sender?.profile_picture_url, let profileUrl = NSURL(string: string) {
-            view.senderImageView.kf_setImageWithURL(profileUrl, placeholderImage: UIImage.randomGhostImage(), optionsInfo: nil, completionHandler: nil)
-        }
-    }
+		view.senderLabel.text = message?.sender?.name
+		view.senderHandleButton.setTitle("@" + (message?.sender?.handle ?? ""), forState: .Normal)
+		if let recipient = message?.recipients?.allObjects.first as? User, let displayName = recipient.name, let handle = recipient.handle {
+			view.recipientLabel.text = "To: " + displayName + " @" + handle
+		}
+		else {
+			view.recipientLabel.text = "To: -"
+		}
+		view.sender = message?.sender
+		view.timeStampeLabel.text = timeFormatter.stringFromDate(message?.sent_at ?? NSDate())
+		if let string = message?.sender?.profile_picture_url, let profileUrl = NSURL(string: string) {
+			view.senderImageView.kf_setImageWithURL(profileUrl, placeholderImage: UIImage.randomGhostImage(), optionsInfo: nil, completionHandler: nil)
+		}
+	}
 
-    func populateView(data message: Message?, view: ThreadMessageTableViewCell, lastMessage: Bool, primary: Bool) {
-        self.populateView(data: message, view: view)
+	func populateView(data message: Message?, view: ThreadMessageTableViewCell, lastMessage: Bool, primary: Bool) {
+		self.populateView(data: message, view: view)
 
-        var bgColor: UIColor?
+		var bgColor: UIColor?
 
-        if (primary) {
-            bgColor = UIColor.whiteColor()
-        }
-        else {
-            bgColor = UIColor(rgba: HexCodes.offWhite)
-        }
+		if (primary) {
+			bgColor = UIColor.whiteColor()
+		}
+		else {
+			bgColor = UIColor(rgba: HexCodes.offWhite)
+		}
 
-        view.contentView.backgroundColor = bgColor
+		view.contentView.backgroundColor = bgColor
 
-        if (lastMessage) {
-            view.separatorContainerHeightConstraint.constant = 0
-        }
-        else {
-            view.separatorContainerHeightConstraint.constant = 44
-        }
-    }
+		if (lastMessage) {
+			view.separatorContainerHeightConstraint.constant = 0
+		}
+		else {
+			view.separatorContainerHeightConstraint.constant = 44
+		}
+	}
 }
