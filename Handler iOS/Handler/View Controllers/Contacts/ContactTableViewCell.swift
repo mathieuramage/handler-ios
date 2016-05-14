4a69152af24a3aa4411d395b373f98331897246b
Cagdas Altinkaya <cagdas@rgbm.co>
2016-05-14 12:42:19 +0300

    Removed commented code.

--- Handler iOS/Handler/View Controllers/Contacts/ContactTableViewCell.swift
+++ Handler iOS/Handler/View Controllers/Contacts/ContactTableViewCell.swift
@@ -9,23 +9,8 @@
 import UIKit
 
 class ContactTableViewCell: UITableViewCell {
-
 	@IBOutlet weak var profileImageView: UIImageView!
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var handleLabel: UILabel!
 	@IBOutlet weak var followButton: UIButton!
-
-//	var user: User? {
-//		didSet {
-//			if let urlString = user?.profile_picture_url, let profileUrl = NSURL(string: urlString) {
-//				self.profileImageView.kf_setImageWithURL(profileUrl, placeholderImage: UIImage.randomGhostImage(), optionsInfo: nil, completionHandler: { (image, error, cacheType, imageURL) -> () in
-//				})
-//			}
-//			self.followButton.setImage(UIImage.imageForTwitterStatus(TwitterFriendshipStatus(rawValue: user?.twtterFollowStatus?.integerValue ?? 2)!), forState: UIControlState.Normal)
-//			self.followButton.enabled = user?.twtterFollowStatus?.integerValue < 2
-//			self.nameLabel.text = user?.name
-//			self.handleLabel.text = user?.handle
-//
-//		}
-//	}
 }
